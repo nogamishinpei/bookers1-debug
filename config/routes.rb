04725740a78a2d3e2d97dd1f8715b2aa6ba85056
EC2 Default User <nogamichiru@gmail.com>
2021-07-30 03:18:53 +0000

    [Finish]bookers1-debug complete

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+    
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books, :homes
+  
+  get 'books/new'
+
+ get 'top' => 'homes#top'
+ post 'books'=>'books#create'
+
+ get 'books'=>'books#index'
+ get 'books/:id' => 'books#show', as: 'show_book'
+ get 'books/:id/edit'=>'books#edit', as: 'editbook'
+patch 'books/:id' => 'books#update', as: 'update_book'
+ delete 'books/:id' => 'books#destroy', as: 'destroy'
+  
+end
